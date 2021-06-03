cb932006cae9f3d2f6c8f0622afb720a0deffa4c
Adriene Smith <adriene.e.smith@gmail.com>
2021-06-03 07:56:50 -0700

    update to index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
-const express = require("express");
-const app = express();
-const productsRouter = require("./products/products.router");
-const categoriesRouter = require("./categories/categories.router");
-const suppliersRouter = require("./suppliers/suppliers.router");
+const { PORT = 5000 } = process.env;
 
-app.use(express.json());
+const app = require("./app");
+const knex = require("./db/connection");
 
-app.use("/products", productsRouter);
-app.use("/categories", categoriesRouter);
-app.use("/suppliers", suppliersRouter);
+const listener = () => console.log(`Listening on Port ${PORT}!`);
 
-app.get("/src", (req, res, next) => {
-  res.status(200).json({ data: "hello world" });
-})
-// Not found handler
-app.use((req, res, next) => {
-  next({ status: 404, message: `Not found: ${req.originalUrl}` });
-});
-
-// Error handler
-app.use((error, req, res, next) => {
-  console.error(error);
-  const { status = 500, message = "Something went wrong!" } = error;
-  res.status(status).json({ error: message });
-});
-
-module.exports = app;
+knex.migrate
+    .latest()
+    .then((migrations) => {
+        console.log("migrations", migrations);
+        app.listen(PORT, listener);
+    })
+    .catch((error) => {
+        console.error(error);
+        knex.destroy();
+    });
